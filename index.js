7c65d7d657e148719aee61e21a2f11a230ddb769
kendall <kendallcoleman@Kendalls-MacBook-Air.local>
2022-12-05 18:07:42 -0500

    1/2 questions

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const util = require("util")
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    messsage:"Enter the title of the project",
-    name: "title",
+    messsage:"Enter the title of the project.",
+    name: "Title",
+    type: "insert",
+}, {
+    messsage:"Leave a detailed description explaining the what, why, and how on your project.",
+    name: "Description",
+    type: "insert",
+}, {
+    messsage:"Add to make more user friendly.",
+    name: "Table of Contents",
+    type: "insert",
+}, {
+    messsage:"What steps must be taken to install this project? Provide a step-by-step description.",
+    name: "Installation",
+    type: "insert",
+}, {
+    messsage:"Provide instructions on how to use app.",
+    name: "Usage",
+    type: "insert",
+}, {
+    messsage:"List any collaborators, third-party assets, or tutorials that helped you complete this project.",
+    name: "Credits",
     type: "insert",
 }, {
     messsage:"Enter the title of the project",
-    name: "title",
-    type: "insert",
-}, {
-    messsage:"Enter the title of the project",
-    name: "title",
-    type: "insert",
-}, {
-    messsage:"Enter the title of the project",
-    name: "title",
-    type: "insert",
-}, {
-    messsage:"Enter the title of the project",
-    name: "title",
-    type: "insert",
-}, {
-    messsage:"Enter the title of the project",
-    name: "title",
-    type: "insert",
-}, {
-    messsage:"Enter the title of the project",
-    name: "title",
+    name: "License",
     type: "insert",
 }, {
     messsage:"Enter the title of the project",
