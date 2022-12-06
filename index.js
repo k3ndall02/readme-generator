ed7b989b14c1b12d5429d18340076a2c6537ff3b
kendall <kendallcoleman@Kendalls-MacBook-Air.local>
2022-12-06 13:50:55 -0500

    started writeToFile function

--- index.js
+++ index.js
@@ -51,7 +51,17 @@ const questions = [{
 }];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("success")
+        } 
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
