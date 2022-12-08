96181c1493812f01accf945ea4e853b1c320e4f0
kendall <kendallcoleman@Kendalls-MacBook-Air.local>
2022-12-08 12:08:08 -0500

    updated markdown

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# Ninja Turtles 
+# ninja turtle 
+
+  <img src="https://img.shields.io/badge/License-GNU-blue.svg"/>
+
 
   ## Description
-  Fights Crime
+  fight crime
 
   ## Table of Contents
   * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
 
+  ## Installation
+  sewer
+
+  ## Usage
+  fight crime
+
+  ## License
+    
+    This project is licensed under GNU
+
+  ## Questions
+  If you have any questions please contact me at [kendallcoleman2023@u.northwestern.edu](mailto:kendallcoleman2023@u.northwestern.edu).
+  For more of my projects check out my github [k3ndall02](https://github.com/k3ndall02).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license!=="None") {
+    return`<img src="https://img.shields.io/badge/License-${license}-blue.svg"/>`
+  }else{
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license!=="None") {
+    return `* [License](#license)`
+  }else{
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license!=="None") {
+    return `## License
+    
+    This project is licensed under ${license}`
+  }else{
+    return ""
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} 
+
+  ${renderLicenseBadge(data.License)}
+
 
   ## Description
   ${data.Description}
 
   ## Table of Contents
   * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.License)}
 
+  ## Installation
+  ${data.Installation}
+
+  ## Usage
+  ${data.Usage}
+
+  ${renderLicenseSection(data.License)}
+
+  ## Questions
+  If you have any questions please contact me at [${data.Email}](mailto:${data.Email}).
+  For more of my projects check out my github [${data.Github}](https://github.com/${data.Github}).
 `;
 }
 
